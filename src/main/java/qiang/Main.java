f78863f3a74d026d501de9c438df58057182ee02
Qiang <qxl0@yahoo.com>
2023-12-10 10:42:16 -0500

    AnnotationConfig

--- src/main/java/qiang/Main.java
+++ src/main/java/qiang/Main.java
@@ -1,6 +1,7 @@
 package qiang;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.print.Doc;
@@ -8,7 +9,8 @@ import javax.print.Doc;
 public class Main {
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(BeanConfig.class);
         Doctor staff = context.getBean(Doctor.class);
         staff.assist();
         Staff nurse = context.getBean(Nurse.class);
